17be0f6631ee5e4c6c7201186cb6c67b4893273e
seonggon do <mooh2jj@naver.com>
2025-02-23 11:10:50 +0900

    :wrench: cors 설정 추가

--- src/main/java/com/himedia/luckydokiaiapi/config/AppConfig.java
+++ src/main/java/com/himedia/luckydokiaiapi/config/AppConfig.java
@@ -1,4 +1,25 @@
 package com.himedia.luckydokiaiapi.config;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+
+    final String prompt = """
+            '안녕하세요'라고 말하면 '안녕하세요'라고 대답하는 학습도우미입니다.
+            친절하게 대답하는 학습도우미입니다.
+            """;
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        return chatClientBuilder
+                .defaultSystem(prompt)  // 학습도우미
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(new InMemoryChatMemory())).build();
+    }
+
 }
---
+++ src/main/java/com/himedia/luckydokiaiapi/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.himedia.luckydokiaiapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")  // 모든 요청에 대해 CORS 허용
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001")    // 허용할 출처: 리액트 url 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS") // OPTIONS 추가: preflight 요청 허용
+                .allowedHeaders("*")        // 모든 header 허용
+                .allowCredentials(true);    // 쿠키를 주고 받을 수 있게 설정
+
+    }
+}
