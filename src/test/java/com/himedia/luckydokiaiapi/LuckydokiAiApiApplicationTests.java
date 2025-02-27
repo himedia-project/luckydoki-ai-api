40db453858cb0c7aefe4631fda6ece5b43728c24
seonggon do <mooh2jj@naver.com>
2025-02-27 22:24:03 +0900

    :memo: 배포시, 테스트코드 지움

--- src/test/java/com/himedia/luckydokiaiapi/LuckydokiAiApiApplicationTests.java
+++ src/test/java/com/himedia/luckydokiaiapi/LuckydokiAiApiApplicationTests.java
@@ -3,11 +3,11 @@ package com.himedia.luckydokiaiapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LuckydokiAiApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class LuckydokiAiApiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
