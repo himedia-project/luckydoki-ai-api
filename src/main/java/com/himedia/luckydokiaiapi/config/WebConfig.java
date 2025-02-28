5daf094242735a6500c0bd14bf5ccd1447cd344f
seonggon do <mooh2jj@naver.com>
2025-02-28 09:59:53 +0900

    :memo: ec2 배포서버 리액트 cors 주소 추가

--- src/main/java/com/himedia/luckydokiaiapi/config/WebConfig.java
+++ src/main/java/com/himedia/luckydokiaiapi/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")  // 모든 요청에 대해 CORS 허용
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")    // 허용할 출처: 리액트 url 주소
+                .allowedOrigins("http://localhost:3000", "http://localhost:3001",
+                        "http://15.165.150.61:3000", "http://15.165.150.61:3001"
+                )    // 허용할 출처: 리액트 url 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS") // OPTIONS 추가: preflight 요청 허용
                 .allowedHeaders("*")        // 모든 header 허용
                 // content-disposition 허용 설정 -> excel,pdf 파일 다운로드시, 제목노출을 위해 필요!
