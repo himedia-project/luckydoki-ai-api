648650c7dcbbc2a351548db050c53b8b418b54b2
seonggon do <mooh2jj@naver.com>
2025-03-03 20:29:31 +0900

    :zap: 유사도 검색 설정, 검색 결과 반환값 더 올림

--- src/main/java/com/himedia/luckydokiaiapi/domain/chatbot/controller/ChatBotController.java
+++ src/main/java/com/himedia/luckydokiaiapi/domain/chatbot/controller/ChatBotController.java
@@ -31,12 +31,12 @@ public class ChatBotController {
         // Fetch similar movies using vector store
         List<Document> results = vectorStore.similaritySearch(
                 SearchRequest.query(question)
-                        .withSimilarityThreshold(0.5)
-                        .withTopK(1)
+                        .withSimilarityThreshold(0.7)
+                        .withTopK(5)
         );
         // vectorStore.similaritySearch(): 벡터 저장소에서 유사도 기반 검색을 수행
         // SearchRequest.query(question): 사용자가 입력한 질문을 검색 쿼리로 사용
-        // 유사도 임계값을 0.5로 설정
+        // 유사도 임계값을 0.5로 설정, 유사도 임계값을 1로 설정하면 정확히 일치하는 결과만 반환
         // withTopK(1): 가장 유사한 상위 1개의 결과만 반환하도록 설정합니다.
         // 만약 .withTopK(5)로 설정하면 상위 5개의 결과를 반환
         // 결과는 List<Document>로 반환되며, 각 Document는 검색된 문서의 내용과 메타데이터를 포함
